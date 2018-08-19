bec8c7cfa1bd7fabb93457680c6163d9b5083714
ZhangGuangYuan <841568119@qq.com>
2018-08-19 20:15:03 +0800

    RelativeLayout and 2 example

---
+++ 11_Android的相对布局/3.相对布局新属性/S08_RelativeLayoutNew/gen/com/yuan/s08_relativelayoutnew/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.yuan.s08_relativelayoutnew;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ 11_Android的相对布局/5.小猪猜拳相对布局/S08_RelativeLayoutPractice2/gen/com/yuan/s08_relativelayoutpractice2/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.yuan.s08_relativelayoutpractice2;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
